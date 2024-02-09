28380463cdf66db829cdfd0f69ec74097990ccff
JahongirBozorov <bozorovjahongir1111@gmail.com>
2024-02-09 15:14:18 +0500

    first commit

--- conf/settings.py
+++ conf/settings.py
@@ -10,8 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import environ
 from pathlib import Path
 
+
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%si34g4@a(&3ee7#p=)to=c&=ebj7o#t%hzryy2t)&+-!35)g$'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -80,16 +84,10 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+import os, dj_database_url
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "Spektor",
-        "USER": "postgres",
-        "PASSWORD": "jahongir3210045",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
+    "default": dj_database_url.parse(env('DATABASE_URL'))
 }
 
 
@@ -128,10 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- teacher/models.py
+++ teacher/models.py
@@ -5,5 +5,5 @@ class Teacher(models.Model):
     objects = None
     name = models.CharField(max_length=255, default=None)
     course = models.CharField(max_length=255)
-    room = models.IntegerField(max_length=15, default=1)
+    room = models.IntegerField(default=1)
     img = models.ImageField(default=None)
